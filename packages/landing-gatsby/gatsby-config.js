0f34b51a250d2e2bc64fa0fdf0b9a0e40d37f397
Suraj Shrestha <syuraj@gmail.com>
2020-03-18 19:44:16 -0400

    fix for gatsby issue with all static images not being created

--- packages/landing-gatsby/gatsby-config.js
+++ packages/landing-gatsby/gatsby-config.js
@@ -4,49 +4,49 @@ module.exports = {
 	siteMetadata: {
 		title: 'A technology company',
 		description: `A technology company`,
-		author: `@SirisTechnology`,
+		author: `@SirisTechnology`
 	},
 	plugins: [
+		`gatsby-plugin-sharp`,
+		`gatsby-transformer-sharp`,
 		{
 			resolve: 'gatsby-plugin-alias-imports',
 			options: {
 				alias: {
 					common: path.resolve(__dirname, '../common'),
-					reusecore: path.resolve(__dirname, '../reusecore'),
-				},
-			},
+					reusecore: path.resolve(__dirname, '../reusecore')
+				}
+			}
 		},
 		{
 			resolve: `gatsby-plugin-styled-components`,
 			options: {
-				minify: false, // Breaks styles if not set to false
-			},
+				minify: false // Breaks styles if not set to false
+			}
 		},
 		`gatsby-plugin-react-helmet`,
 		{
 			resolve: `gatsby-source-filesystem`,
 			options: {
 				name: `images`,
-				path: `${__dirname}/src/images`,
-			},
+				path: `${__dirname}/src/images`
+			}
 		},
 		{
 			resolve: `gatsby-source-filesystem`,
 			options: {
 				name: `common`,
-				path: `../common/src/assets/`,
-			},
+				path: `../common/src/assets/`
+			}
 		},
 		`gatsby-transformer-json`,
 		{
 			resolve: `gatsby-source-filesystem`,
 			options: {
 				name: `data`,
-				path: `../common/src/data/`,
-			},
+				path: `../common/src/data/`
+			}
 		},
-		`gatsby-transformer-sharp`,
-		`gatsby-plugin-sharp`,
 		{
 			resolve: `gatsby-plugin-manifest`,
 			options: {
@@ -56,8 +56,8 @@ module.exports = {
 				background_color: `#663399`,
 				theme_color: `#663399`,
 				display: `minimal-ui`,
-				icon: `src/images/favicon.png`, // This path is relative to the root of the site.
-			},
+				icon: `src/images/favicon.png` // This path is relative to the root of the site.
+			}
 		},
 		{
 			resolve: `gatsby-plugin-prefetch-google-fonts`,
@@ -65,34 +65,47 @@ module.exports = {
 				fonts: [
 					{
 						family: `Roboto`,
-						variants: [`100`, `100i`, `300`, `300i`, `400`, `400i`, `500`, `500i`, `700`, `700i`, `900`, `900i`],
+						variants: [
+							`100`,
+							`100i`,
+							`300`,
+							`300i`,
+							`400`,
+							`400i`,
+							`500`,
+							`500i`,
+							`700`,
+							`700i`,
+							`900`,
+							`900i`
+						]
 					},
 					{
 						family: `Poppins`,
-						variants: [`300`, `400`, `500`, `600`, `700`],
+						variants: [`300`, `400`, `500`, `600`, `700`]
 					},
 					{
 						family: `Lato`,
-						variants: [`300`, `400`, `700`],
+						variants: [`300`, `400`, `700`]
 					},
 					{
 						family: `Open Sans`,
-						variants: [`300`, `400`, `600`, `700`, `800`],
+						variants: [`300`, `400`, `600`, `700`, `800`]
 					},
 					{
 						family: `Raleway`,
-						variants: [`500`, `600`],
+						variants: [`500`, `600`]
 					},
 
 					{
 						family: `Heebo`,
-						variants: [`300`, `400`, `500`, `600`, `700`, `800`],
-					},
-				],
-			},
-		},
+						variants: [`300`, `400`, `500`, `600`, `700`, `800`]
+					}
+				]
+			}
+		}
 		// this (optional) plugin enables Progressive Web App + Offline functionality
 		// To learn more, visit: https://gatsby.app/offline
 		// 'gatsby-plugin-offline',
-	],
+	]
 }
