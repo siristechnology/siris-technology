2766d23a3a5a1e85906a9a9c6b382fc6a174da01
Suraj Shrestha <syuraj@gmail.com>
2020-10-13 02:15:01 -0400

    Google rating bar

--- packages/landing-gatsby/src/containers/AppClassic/Banner/index.js
+++ packages/landing-gatsby/src/containers/AppClassic/Banner/index.js
@@ -5,16 +5,24 @@ import Image from 'reusecore/src/elements/Image'
 import Link from 'reusecore/src/elements/Link'
 import Heading from 'reusecore/src/elements/Heading'
 import Container from 'common/src/components/UI/Container'
-import BannerWrapper, { BannerContent, BannerImage, ButtonGroup } from './banner.style'
+import BannerWrapper, { BannerContent, BannerImage, ButtonGroup, RatingInfo } from './banner.style'
+import Rating from 'common/src/components/Rating'
 
 import bannerImg from 'common/src/assets/image/appClassic/bannerImg.webp'
 import googlePlayBadge from 'common/src/assets/image/appClassic/google-play-badge.png'
+import google from 'common/src/assets/image/appClassic/google.svg'
 
 const Banner = () => {
 	return (
 		<BannerWrapper id="home">
 			<Container>
 				<BannerContent>
+					<Fade up>
+						<RatingInfo>
+							<Rating rating={4} />
+							4.8 of 5 on <img src={google} alt="Google" height="20" />
+						</RatingInfo>
+					</Fade>
 					<Fade up delay={100}>
 						<Heading as="h1" content="NepalToday App is now available in Android Store" />
 					</Fade>
@@ -27,7 +35,7 @@ const Banner = () => {
 								href="https://play.google.com/store/apps/details?id=com.siristechnology.nepaltodayapp"
 								target="_blank"
 							>
-								<Image src={googlePlayBadge} alt="Banner" style={{ height: 60 }} />
+								<Image src={googlePlayBadge} alt="Banner" style={{ height: 80 }} />
 							</Link>
 						</ButtonGroup>
 					</Fade>
